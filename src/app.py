8da67fb65657582e682f3a3c3e1755c1e9723ed0
leoswbj <leonardo.silva@webjump.ai>
2025-06-25 13:46:52 +0000

    Adiciona validação para impedir que alunos se inscrevam em atividades duplicadas

--- src/app.py
+++ src/app.py
@@ -102,6 +102,7 @@ def signup_for_activity(activity_name: str, email: str):
     activity = activities[activity_name]
 
     # Add student
+    # Validar se o aluno já está inscrito
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up for this activity")
     activity["participants"].append(email)
